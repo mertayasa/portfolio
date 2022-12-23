72753d84e67f750a3754f1996aff50b1504f28b9
bali.appkey <bali.appkey@gmail.com>
2022-12-23 17:05:56 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <button @click="count++" type="button">+</button>
+    <h3>{{ count }}</h3>
+  </main>
+</template>
+
+<script lang="ts">
+  export default {
+    data() {
+      return {
+        count: 33
+      }
+    }
+  }
+</script>

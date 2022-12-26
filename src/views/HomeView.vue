eee24c40f5bcceeabd8262b2d8a88fb1b402cde7
bali.appkey <bali.appkey@gmail.com>
2022-12-26 16:49:56 +0800

    Update : Add tailwinf

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import TheWelcome from "../components/TheWelcome.vue";
-</script>
+</script> -->
 
 <template>
   <main>
-    <TheWelcome />
-    <button @click="count++" type="button">+</button>
+    <button @click="count++" class="inline-block text-green-900 border border-green-500 bg-green-400 px-12 py-3 rounded font-bold transition-all hover:bg-transparent hover:text-green-500 focus:outline-none focus:ring active:text-green-500" type="button">Add More</button>
     <h3>{{ count }}</h3>
   </main>
 </template>
